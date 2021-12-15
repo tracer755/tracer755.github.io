356d1bba045be554922f8a0ba7c6bcf7c6f24ba0
Jason Putland <44651512+tracer755@users.noreply.github.com>
2021-12-15 08:47:18 -0700

    edit

--- MelodicaJoin.js
+++ MelodicaJoin.js
@@ -1,1 +1,30 @@
-confetti();
+
+var colors = ['#5865F2', '#505cde'];
+
+// do this for 30 seconds
+var duration = 2.5 * 1000;
+var end = Date.now() + duration;
+
+(function frame() {
+  // launch a few confetti from the left edge
+  confetti({
+    particleCount: 5,
+    angle: 60,
+    spread: 65,
+    origin: { x: 0 },
+    colors: colors
+  });
+  // and launch a few from the right edge
+  confetti({
+    particleCount: 5,
+    angle: 120,
+    spread: 65,
+    origin: { x: 1 },
+    colors: colors
+  });
+
+  // keep going until we are out of time
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+}());
