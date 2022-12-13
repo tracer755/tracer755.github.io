65f5955c3aceaa30331428fab5da59873dd513ab
tracer755 <jason@klasstek.com>
2022-12-13 09:17:55 -0700

    update

--- prs/js/headder-footer.js
+++ prs/js/headder-footer.js
@@ -16,7 +16,7 @@ const FooterCode = `
 
 const HeaderCode = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">Prs Pics</a>
+    <a class="navbar-brand" href="/prs/">Prs Pics</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -24,7 +24,7 @@ const HeaderCode = `
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
+            <a class="nav-link" href="/prs/">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="random.html">Random</a>
