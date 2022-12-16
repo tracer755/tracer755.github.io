68623f4da67db07d60ec7bd25a49d296649f6a53
tracer755 <jason@klasstek.com>
2022-12-15 22:58:21 -0700

    Removed Prs Pics

--- prs/js/random.js
+++
@@ -1,5 +0,0 @@
-axios.get('https://cute-tan-bear-tie.cyclic.app/random')
-.then(function (response) {
-    console.log(response.data);
-    document.getElementById('rnd-photo').src = response.data;
-})
