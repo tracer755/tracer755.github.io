b0fbc3d1a36ca956fade481811c00129bb14625d
Tracer755 <44651512+tracer755@users.noreply.github.com>
2023-04-27 17:37:46 -0600

    T456 TEST removed
    Test site for t456 has been removed from this domain

--- t456/js/404.js
+++
@@ -1,7 +0,0 @@
-let bgImg = ["Img/404_Back3.webp", "Img/404_Back2.webp", "Img/404_Back3.webp", "Img/404_Back4.webp", "Img/404_Back5.webp", "Img/404_Back6.webp", "Img/404_Back3.webp"]
-let ImgPointer = getRandomInt(bgImg.length);
-document.getElementById("404Background").style.backgroundImage = "url('" + bgImg[ImgPointer] + "')";
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
